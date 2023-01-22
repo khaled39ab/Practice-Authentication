ee6e60a577a3f63e0e46f61c7a1f2823e77dc218
khaled39ab <khaled39ab@gmail.com>
2023-01-22 23:22:25 +0600

    file created

---
+++ src/components/SignIn/SocialSignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialSignIn = () => {
+    return (
+        <div>
+            <h1>Social Sign In</h1>
+        </div>
+    );
+};
+
+export default SocialSignIn;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbfvJyQz4IPQHvSUr-k7x1Js-d2Nle3D0",
+  authDomain: "authentication-practice-8ffe6.firebaseapp.com",
+  projectId: "authentication-practice-8ffe6",
+  storageBucket: "authentication-practice-8ffe6.appspot.com",
+  messagingSenderId: "1026174988655",
+  appId: "1:1026174988655:web:0dcba7f3028cf8b242789a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
