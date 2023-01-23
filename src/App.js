c1afa2e64f172c8b8828652de55c35343e9649cb
khaled39ab <khaled39ab@gmail.com>
2023-01-23 12:29:01 +0600

    social log in page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SocialSignIn from './components/SignIn/SocialSignIn';
 
 function App() {
   return (
     <div className="App">
       <h1>Authentication Practice</h1>
+      <SocialSignIn />
     </div>
   );
 }
--- src/components/SignIn/SocialSignIn.js
+++ src/components/SignIn/SocialSignIn.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { GoogleAuthProvider } from "firebase/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const SocialSignIn = () => {
+    const googleProvider = new GoogleAuthProvider();
+
+    const signInWithGoogle = () => {
+
+    }
+
+    const signInWithFacebook = () => {
+
+    }
+
+    const signInWithGithub = () => {
+
+    }
+
     return (
         <div>
             <h1>Social Sign In</h1>
+            
+            <div>
+                <button onClick={() => { signInWithGoogle() }} className='btn btn-info mx-auto w-50
+            d-block mb-2'>
+                    <span className='pe-2'>
+                        <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+                    </span>
+                    Sign In With Google
+                </button>
+                <button onClick={() => signInWithFacebook()} className='btn btn-primary mx-auto w-50 d-block mb-2'>
+                    <span className='pe-2'>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </span>
+                    Sign In With Facebook
+                </button>
+                <button onClick={() => signInWithGithub()} className='btn btn-dark mx-auto w-50 d-block'>
+                    <span className='pe-2'>
+                        <FontAwesomeIcon icon={faGithub} />
+                    </span>
+                    Sign In With Github
+                </button>
+            </div>
         </div>
     );
 };
-
 export default SocialSignIn;
