3c58d08bc76b2e00b8a0ced764ba4daa6e8953a9
khaled39ab <khaled39ab@gmail.com>
2023-01-27 23:44:00 +0600

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1 style={{color: 'white'}}>Authentication Practice</h1>
       <SignIn />
       <SignUp />
