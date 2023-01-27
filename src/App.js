836d3aa0dfe11e9cce777302c2514c60142b66f1
khaled39ab <khaled39ab@gmail.com>
2023-01-27 11:03:46 +0600

    signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
       <h1>Authentication Practice</h1>
       <SignIn />
+      <SignUp />
     </div>
   );
 }
