972d6cff5ef9a76df1af6a2216d8a4cd19802437
khaled39ab <khaled39ab@gmail.com>
2023-01-29 12:25:16 +0600

    email password authentication working

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,38 +1,61 @@
 import { faArrowRight, faAt, faLock, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    const auth = getAuth();
+
+    const handleSignUp = e => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const phone = form.phone.value;
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(res => {
+                const user = res.user;
+                
+                console.log(user);
+            })
+            .catch(err => {
+                console.error(err.message);
+            })
+
+        form.reset();
+    }
     return (
         <div className="container">
             <div className="screen">
                 <div className="screen__content">
-                    <form className="signUp">
+                    <form className="signUp" onSubmit={handleSignUp}>
                         <div className="signUp__field">
                             <FontAwesomeIcon icon={faUser} className="signIn__icon" />
-                            <input type="text" className="signUp__input" placeholder="Enter Your Name " />
+                            <input type="text" name='name' className="signUp__input" placeholder="Enter Your Name " required />
                         </div>
                         <div className="signUp__field">
                             <FontAwesomeIcon icon={faAt} className="signIn__icon" />
-                            <input type="text" className="signUp__input" placeholder="Enter Your Email" />
+                            <input type="text" name='email' className="signUp__input" placeholder="Enter Your Email" required />
                         </div>
                         <div className="signUp__field">
                             <FontAwesomeIcon icon={faLock} className="signIn__icon" />
-                            <input type="password" className="signUp__input" placeholder="Enter Password" />
+                            <input type="password" name='password' className="signUp__input" placeholder="Enter Password" required />
                         </div>
                         <div className="signUp__field">
                             <FontAwesomeIcon icon={faPhone} className="signIn__icon" />
-                            <input type="number" className="signUp__input" placeholder="Enter Your Number" />
+                            <input type="number" name='phone' className="signUp__input" placeholder="Enter Your Number" required />
                         </div>
-                        <button className="button signUp__submit">
+                        <button type='submit' className="button signUp__submit">
                             <span className="button__text">Sign Up</span>
                             <FontAwesomeIcon icon={faArrowRight} className="button__icon" />
                         </button>
                     </form>
                     <div className='signUp-new-acc'>
-                        <h4 style={{marginBottom: '.5rem'}}>Have An Account?</h4>
+                        <h4 style={{ marginBottom: '.5rem' }}>Have An Account?</h4>
                         <NavLink to='/signIn' className='link-signIn'>Sign In</NavLink>
                     </div>
                 </div>
