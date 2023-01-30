4dad3596b9e15699312c7f6e259b76633d9ab3b4
khaled39ab <khaled39ab@gmail.com>
2023-01-30 23:59:04 +0600

    use auth context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,13 +2,13 @@ import React, { createContext } from 'react';
 
 const AuthContext = createContext();
 
-const UserContext = () => {
+const UserContext = ({children}) => {
     const handleAuth = {};
 
     return (
-        <AuthContext value={handleAuth}>
-
-        </AuthContext>
+        <AuthContext.Provider value={handleAuth}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
