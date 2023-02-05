5ee253016f76cee8f388db8a0081a7e3d6eecf4f
khaled39ab <khaled39ab@gmail.com>
2023-02-05 21:50:49 +0600

    added some page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home Page/Home';
 import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import './App.css';
@@ -13,6 +15,8 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
         <Route path='/signIn' element={<SignIn />}></Route>
         <Route path='/signUp' element={<SignUp />}></Route>
       </Routes>
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Here is  Contact</h1>
+        </div>
+    );
+};
+
+export default Contact;
